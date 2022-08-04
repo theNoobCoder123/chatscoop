0714d3e553e075bdbbc893dab81c4bb44b3814a3
theNoobCoder123 <patelhrishikesh2000@gmail.com>
2022-08-04 06:32:16 +0530

    feat: Created simple backend that allows to create a public chat room.

---
+++ src/main/java/com/example/chatscoop/ChatscoopApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatscoop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatscoopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatscoopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chatscoop/ChatscoopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatscoop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatscoopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
